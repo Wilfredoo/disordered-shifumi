68f205eaea863f9dae01aa4f9f0a23b09c1f134a
Wilfredo <casas.farach@gmail.com>
2020-10-19 13:25:19 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+ const winOrLose = (myChoice, computerChoice) => {
+  setDraw(false);
+  (myChoice === "Scissors" && computerChoice === "Paper") ||
+  (myChoice === "Paper" && computerChoice === "Rock") ||
+  (myChoice === "Rock" && computerChoice === "Scissors")
+    ? setPlayerScore((playerScore) => playerScore + 1)
+    : setComputerScore((computerScore) => computerScore + 1);
+};
+
 
 function App() {
+  import React, { useState } from "react";
+
+  const generateComputerChoice = () => {
+    const randomElement = elements[Math.floor(Math.random() * elements.length)];
+    return randomElement;
+  };
+
+ 
+  import "./App.css";
+
+
+  <button onClick={() => drawOrNot("Rock")}>Rock</button>
+      <button onClick={() => drawOrNot("Paper")}>Paper</button>
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <button onClick={() => drawOrNot("Scissors")}>Scissors</button>
+      <p>Player Score: {playerScore}</p>
+      <p>Computer Score: {computerScore}</p>
+      {draw && <p>It's a draw</p>}
+
+
+      const drawOrNot = (myChoice) => {
+    const computerChoice = generateComputerChoice();
+    computerChoice === myChoice
+      ? setDraw(true)
+      : winOrLose(myChoice, computerChoice);
+  };
+
     </div>
   );
 }
 
 export default App;
+
+const [playerScore, setPlayerScore] = useState(0);
+const [computerScore, setComputerScore] = useState(0);
+const [draw, setDraw] = useState(false);
+const elements = ["Rock", "Paper", "Scissors"];
